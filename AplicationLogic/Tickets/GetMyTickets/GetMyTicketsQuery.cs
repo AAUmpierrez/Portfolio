b0a4c039dd297a56f45faa386f4f455d57eccb18
AAUmpierrez <aa276245@fi365.ort.edu.uy>
2026-03-17 13:03:35 -0300

    getMyTickets_queryHandler

---
+++ AplicationLogic/Tickets/GetMyTickets/GetMyTicketsQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SharedLogic.DTOs.Ticket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicationLogic.Tickets.GetMyTickets
+{
+    public class GetMyTicketsQuery:IRequest<List<TicketListDto>>
+    {
+        public int? Status { get; set; }
+        public int? Priority { get; set; }
+        public int CurrentUserId { get; set; }
+    }
+}
---
+++ AplicationLogic/Tickets/GetMyTickets/GetMyTicketsQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using BussinesLogic.Enums;
+using BussinesLogic.RepositoryInterfaces;
+using MediatR;
+using SharedLogic.DTOs.Ticket;
+using SharedLogic.Exceptions;
+using SharedLogic.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicationLogic.Tickets.GetMyTickets
+{
+    public class GetMyTicketsQueryHandler:IRequestHandler<GetMyTicketsQuery,List<TicketListDto>>
+    {
+        private readonly ITicketRepository _ticketRepository;
+        public GetMyTicketsQueryHandler(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+        public async Task<List<TicketListDto>> Handle(GetMyTicketsQuery request, CancellationToken cancellationToken)
+        {
+            if (request == null) throw new BadRequestException("Query not valid");
+            var tickets = await _ticketRepository.GetAllAsync();
+            var query = tickets.AsQueryable();
+            query = query.Where(t => t.CreatorUserId == request.CurrentUserId);
+
+            if (request.Status.HasValue)
+            {
+                query = query.Where(t => t.State == (TicketState)request.Status);
+            }
+            if (request.Priority.HasValue)
+            {
+                query = query.Where(t => t.Priority == (TicketPriority)request.Priority);
+            }
+
+            return TicketMapper.TicketsToTicketListDto(query.ToList()).ToList();
+        }
+    }
+}
